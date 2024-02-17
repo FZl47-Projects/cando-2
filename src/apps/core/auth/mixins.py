b3180ff566a9f413c90d29ea4a7b52ee64aa0968
AmirMahdi Azarboon <amirazarboon@gmail.com>
2024-02-18 01:06:16 +0330

    Add admin dashboard template

--- src/apps/core/auth/mixins.py
+++ src/apps/core/auth/mixins.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.mixins import AccessMixin
+from django.http import HttpResponseForbidden
 from django.shortcuts import redirect
 
 
@@ -10,4 +11,17 @@ class LoginRequiredMixinCustom(AccessMixin):
         if not request.user.is_phonenumber_confirmed:
             return redirect('account:confirm_phonenumber')
         
+        return super().dispatch(request, *args, **kwargs)
+
+
+class AdminRequiredMixin:
+    """ Admin users access only """
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_anonymous:
+            return redirect('account:login_register')
+
+        if not request.user.is_admin:
+            return HttpResponseForbidden()
+
         return super().dispatch(request, *args, **kwargs)
--- src/apps/dashboard/views/admin.py
+++ src/apps/dashboard/views/admin.py
@@ -1,7 +1,7 @@
 from django.views.generic import View, TemplateView, DetailView
-from apps.core.auth.mixins import LoginRequiredMixinCustom
+from apps.core.auth.mixins import AdminRequiredMixin
 
 
 # Render AdminDashboard view
-class AdminDashboardView(LoginRequiredMixinCustom, TemplateView):
+class AdminDashboardView(AdminRequiredMixin, TemplateView):
     template_name = 'dashboard/admin/index.html'
