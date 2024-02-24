4226d1803a7effbe144d7eeba2fcc9b12b632a6b
AmirMahdi Azarboon <amirazarboon@gmail.com>
2024-02-24 20:55:28 +0330

    Add: Address model & admin

--- src/apps/navigation/admin.py
+++ src/apps/navigation/admin.py
@@ -1,3 +1,16 @@
+from django.utils.translation import gettext as _
 from django.contrib import admin
+from .models import Address
 
-# Register your models here.
+
+# Register Address Admin
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ('id', 'get_phone_number', 'province', 'city', 'active')
+    list_display_links = ('id', 'get_phone_number')
+    list_filter = ('active',)
+
+    @admin.display(description=_('Phone number'))
+    def get_phone_number(self, obj):
+        if obj.id:
+            return str(obj.user.get_phone_number())
--- src/apps/navigation/models.py
+++ src/apps/navigation/models.py
@@ -1,3 +1,23 @@
+from django.utils.translation import gettext as _
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+from apps.core.models import BaseModel
+User = get_user_model()
+
+
+# Addresses model
+class Address(BaseModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='addresses', verbose_name=_('User'))
+    province = models.CharField(_('Province'), max_length=128, null=True, blank=True)
+    city = models.CharField(_('City'), max_length=128, null=True, blank=True)
+    address_line = models.CharField(_('Address'), max_length=255)
+    active = models.BooleanField(_('Active address'), default=False)
+
+    class Meta:
+        verbose_name = _('Address')
+        verbose_name_plural = _('Addresses')
+        ordering = ('-id',)
+
+    def __str__(self):
+        return f'{self.user} - {self.province} - {self.city}'
