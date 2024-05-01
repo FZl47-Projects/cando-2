baa9261b3717b10af3bf7b8425b0ae92a0cf1f0d
root <root@DsIH1QB6waES0iBEKCwu.epanel-info.info>
2024-05-01 06:38:55 +0000

    add startup config production

---
+++ startup/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd /root/projects/cando-2
+source venv/bin/activate
+
+nohup uwsgi --ini startup/uwsgi.ini > ./logs/server.log  &
+
+nohup python src/manage.py qcluster > ./logs/qcluster.log &
